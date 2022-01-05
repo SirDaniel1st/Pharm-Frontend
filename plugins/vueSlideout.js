fa5bf594a69e1b77d61a708e73dc96a9c18ce2d3
SirDaniel1st <danrash123@gmail.com>
2022-01-05 05:28:49 +0000

    Dependencies upgraded to latest

--- plugins/vueSlideout.js
+++ plugins/vueSlideout.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Slideout from '@hyjiacan/vue-slideout'
-import '@hyjiacan/vue-slideout/lib/slideout.css'
+
 
 // import Slideout component, and set the defaults props
 Vue.use(Slideout, {
